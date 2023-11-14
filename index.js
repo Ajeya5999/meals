ab66db92ca42690371112ce4552fd6769e2d1829
Ajeya5999 <ajeyasengar@gmail.com>
2023-11-14 19:41:56 +0530

    Thrid Commit: Added search bar

---
+++ index.js
@@ -0,0 +1,20 @@
+// HTML Items
+
+const mealList = document.getElementById("meal-list"); //the unorder meal list from html
+const searchedMeal = document.getElementById("search-meals-bar-input")
+const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" // api's link to retrieve data
+let data; //Meals data array
+
+// IIFE
+
+// (function() {
+//     data = 
+// })
+
+// Functions
+
+    //Function for fetching data and displaying it 
+
+    function fetchDataAndDisplay() {
+
+    }
